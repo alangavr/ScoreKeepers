1d5a5b6424530c3b62fc6652111b78d70a20a945
Vibha <alangavr@rose-hulman.edu>
2018-03-21 10:01:24 -0400

    basic .h and .m files added

---
+++ ScoreKeepers/ObjCScoreKeeperViewController.h
@@ -0,0 +1,32 @@
+//
+//  ObjCScoreKeeperViewController.h
+//  ScoreKeepers
+//
+//  Created by CSSE Department on 3/20/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObjCScoreKeeperViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel* roundLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField* player1ScoreTextField;
+@property (weak, nonatomic) IBOutlet UITextField* player2ScoreTextField;
+@property (weak, nonatomic) IBOutlet UITextField* player3ScoreTextField;
+@property (weak, nonatomic) IBOutlet UITextField* player4ScoreTextField;
+
+@property (weak, nonatomic) IBOutlet UITextView* player1ScoreTextView;
+@property (weak, nonatomic) IBOutlet UITextView* player2ScoreTextView;
+@property (weak, nonatomic) IBOutlet UITextView* player3ScoreTextView;
+@property (weak, nonatomic) IBOutlet UITextView* player4ScoreTextView;
+
+
+- (IBAction)pressedEnterRound:(id)sender;
+
+
+- (IBAction)pressedNewGame:(id)sender;
+
+
+@end
---
+++ ScoreKeepers/ObjCScoreKeeperViewController.m
@@ -0,0 +1,34 @@
+//
+//  ObjCScoreKeeperViewController.m
+//  ScoreKeepers
+//
+//  Created by CSSE Department on 3/20/18.
+//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//
+
+#import "ObjCScoreKeeperViewController.h"
+
+@implementation ObjCScoreKeeperViewController
+
+//NSString* textInTextField = self.editTextField.text;
+//int textAsInt = [textInTextField intValue];
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pressedEnterRound:(id) sender {
+
+}
+
+- (IBAction)pressedNewGame:(id) sender {
+
+}
+@end;
